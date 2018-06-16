a19ca11095c27e7d7599be49c9b7b8180ef5980a
everbrez <everbrez@gmail.com>
2018-06-16 17:48:41 +0800

    登录页面结构

--- src/app.jsx
+++ src/app.jsx
@@ -1,7 +1,18 @@
 import React from 'react';
-import ReactDOM from "react-dom";
+import ReactDOM from 'react-dom'
+import Login from './components/login/login'
+import './assets/style/reset.scss'
+import Grid from '@material-ui/core/Grid';
+
+const element = (
+    <Grid container justify='center'>
+        <Grid item xs={2}>
+        <Login> </Login>
+        </Grid>
+    </Grid>
+)
 
 ReactDOM.render(
-   <h1>Hello World!</h1>,
+    element,
     document.body
 );
---
+++ src/components/login/login.jsx
@@ -0,0 +1,71 @@
+import React from 'react';
+import ReactDOM from 'react-dom'
+import { withStyles } from '@material-ui/core/styles'
+import TextField from '@material-ui/core/TextField';
+import Grid from '@material-ui/core/Grid';
+import Icon from '@material-ui/core/Icon';
+import Button from '@material-ui/core/Button';
+import LinearProgress from '@material-ui/core/LinearProgress';
+import Checkbox from '@material-ui/core/Checkbox';
+import FormControlLabel from '@material-ui/core/FormControlLabel';
+
+class Login extends React.Component {
+    constructor(props) {
+        super()
+        this.props = props
+    }
+    render() {
+        return (
+            <Grid container spacing={20}>
+                <Grid item xs={12}>
+                    <LinearProgress variant="buffer" value={30} valueBuffer={33} />
+                </Grid>
+                <Grid item xs={12}>
+                    <Grid container
+                        alignItems='center'
+                        justify='center'>
+                        <TextField id='account' type='text' label='account' margin='dense' fullWidth />
+                    </Grid>
+                </Grid>
+                <Grid item xs={12}>
+                    <Grid container
+                        alignItems='center'
+                        justify='center'>
+                        <TextField id='account' type='password' label='password' margin='dense' fullWidth />
+                    </Grid>
+                </Grid>
+                <Grid item xs={12}>
+                    <Grid container
+                        alignItems='center'
+                        justify='space-between'>
+                        <Grid item >
+                            <FormControlLabel
+                                control={
+                                    <Checkbox
+                                        checked={true}
+                                        value="checkedB"
+                                        color="primary"
+                                    />
+                                }
+                                label="remeber password"
+                            />
+                        </Grid>
+                        <Grid item>
+                            forget password
+                        </Grid>
+                    </Grid>
+                </Grid>
+
+                <Grid item xs={12}>
+                    <Grid container
+                        alignItems='center'
+                        justify='space-around'>
+                        <Button variant="contained" color="primary" className={this.props.button}>Login</Button>
+                    </Grid>
+                </Grid>
+            </Grid>
+        )
+    }
+}
+
+export default withStyles()(Login)
